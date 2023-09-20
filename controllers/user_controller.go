99fe93112411968700a484cad35ad24fa75786e6
Yugesh Palvai <yugeshpalvai21@gmail.com>
2023-09-20 23:14:15 +0530

    generates users controller with action that can be called from routes and return json response

---
+++ controllers/user_controller.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type UserController struct{}
+
+func (uc *UserController) GetUserByID(w http.ResponseWriter, r http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	resp := make(map[string]string)
+	resp["message"] = "Status OK"
+
+	jsonResp, err := json.Marshal(resp)
+
+	if err != nil {
+		fmt.Println("There is error in controler")
+	}
+
+	w.Write(jsonResp)
+
+	return
+}
